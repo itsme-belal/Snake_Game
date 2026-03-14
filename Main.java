1bf32bf5d9b6d22567279d20bf8a78c01ec54a50
Belal Hossain <itsmebelalhossain@gmail.com>
2026-03-15 01:08:25 +0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+import java.awt.Color;
+
+import javax.swing.JFrame;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		JFrame frame = new JFrame();
+		frame.setTitle("Snake Game");
+		frame.setBounds(10, 10, 905, 700);
+		frame.setResizable(false);
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setBackground(Color.DARK_GRAY);
+
+		GamePanel panel = new GamePanel();
+		panel.setBackground(Color.DARK_GRAY);
+		frame.add(panel);
+
+
+		frame.setVisible(true);
+	}
+
+}
+
